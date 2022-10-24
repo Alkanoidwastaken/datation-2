f4f73cf184474a91324d5e143693934a76b0d69c
alkanoid <thinuka@de-silva.com.au>
2022-10-24 22:38:49 +0800

    Fix to compile.py

--- compile.py
+++ compile.py
@@ -10,5 +10,4 @@ def import_or_install(package):
 import_or_install('cpython')
 
 os.system('cython main.py --embed')
-os.system("""PYTHONLIBVER=python$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')$(python3-config --abiflags)""")
-os.system("""gcc -Os $(python3-config --includes) main.c -o output_bin_file $(python3-config --ldflags) -l$PYTHONLIBVER""")
+os.system("""PYTHONLIBVER=python$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')$(python3-config --abiflags) gcc -Os $(python3-config --includes) main.c -o output_bin_file $(python3-config--ldflags)-l$PYTHONLIBVER""")
